5a0d9e4de256afcff08e7c7d5e997d4aa605e2e6
Dick Brouwers <dickbrouwers@Dicks-MacBook-Pro.local>
2018-06-18 23:06:29 +0200

    green

--- src/Grid.php
+++ src/Grid.php
@@ -4,7 +4,30 @@ declare(strict_types = 1);
 
 namespace Dojo\GameOfLife;
 
-final class Grid
+final class Grid implements \Iterator
 {
+    public function current()
+    {
+        // TODO: Implement current() method.
+    }
 
+    public function next()
+    {
+        // TODO: Implement next() method.
+    }
+
+    public function key()
+    {
+        // TODO: Implement key() method.
+    }
+
+    public function valid()
+    {
+        // TODO: Implement valid() method.
+    }
+
+    public function rewind()
+    {
+        // TODO: Implement rewind() method.
+    }
 }
