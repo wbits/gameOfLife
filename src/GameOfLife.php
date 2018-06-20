d05d6f726cfc59b7c597cfc24224c52aa10a038f
Dick Brouwers <dick@insided.nl>
2018-06-20 09:23:34 +0200

    green

--- src/GameOfLife.php
+++ src/GameOfLife.php
@@ -6,7 +6,15 @@ namespace Dojo\GameOfLife;
 
 final class GameOfLife
 {
+    private $grid;
+
+    public function __construct(Grid $grid)
+    {
+        $this->grid = $grid;
+    }
+
     public static function firstGeneration(int $width, int $height): GameOfLife
     {
+        return new self(new Grid($width, $height));
     }
 }
