3f87e83771636bc522cf5128b9522415dcad4ee0
Dick Brouwers <dick@insided.nl>
2018-06-25 08:54:08 +0200

    added lifecycle rules

--- tests/CellTest.php
+++ tests/CellTest.php
@@ -24,4 +24,67 @@ final class CellTest extends TestCase
 
         self::assertEquals(1, $cell->livingNeighbours());
     }
+
+    public function testALiveCellWithFewerThanTwoLiveNeighboursDies()
+    {
+        $cell = new Cell(true);
+
+        $nextGeneration = Cell::nextGeneration($cell);
+
+        self::assertFalse($nextGeneration->isAlive());
+    }
+
+    public function testAnyLiveCellWithMoreThanThreeLiveNeighboursDies()
+    {
+        $cell = new Cell(true);
+        $livingNeighbour = new Cell(true);
+
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+        $nextGeneration = Cell::nextGeneration($cell);
+
+        self::assertFalse($nextGeneration->isAlive());
+    }
+
+    public function testAnyLiveCellWithTwoOrThreeLiveNeighboursLives()
+    {
+        $cell = new Cell(true);
+        $livingNeighbour = new Cell(true);
+
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+
+        $nextGeneration = Cell::nextGeneration($cell);
+
+        self::assertTrue($nextGeneration->isAlive());
+    }
+
+    public function testAnyDeadCellWithExactlyThreeLiveNeighboursBecomesLive()
+    {
+        $cell = new Cell(false);
+        $livingNeighbour = new Cell(true);
+
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+
+        $nextGeneration = Cell::nextGeneration($cell);
+
+        self::assertTrue($nextGeneration->isAlive());
+    }
+
+    public function testAnyDeadCellWithNotExactlyThreeLiveNeighboursRemainsDead()
+    {
+        $cell = new Cell(false);
+        $livingNeighbour = new Cell(true);
+
+        $cell->registerStatus($livingNeighbour);
+        $cell->registerStatus($livingNeighbour);
+
+        $nextGeneration = Cell::nextGeneration($cell);
+
+        self::assertFalse($nextGeneration->isAlive());
+    }
 }
